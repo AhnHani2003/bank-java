28bd349be9f9ddb7d8df442fa5362215710753e8
Trần Thông <145345242+AhnHani2003@users.noreply.github.com>
2024-06-24 12:00:36 +0700

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,132 +1,76 @@
 use master
 create database BANK
-on primary
-/****** Object:  Database [BANK]*/
+/****** Object:  Database BANK*/
 GO
-USE [BANK]
+USE BANK
 GO
 /****** Object:  Table account*/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE account(
-	[accountid] [int] IDENTITY(10000000,1) NOT NULL,
-	[customerid] [int] NOT NULL,
-	[password] [varchar](50) NOT NULL,
-	[money] [float] NOT NULL,
-	[loan] [float](50) NULL,
-	[saving] [float] NULL,
-	[notiID] [int] NULL,
- CONSTRAINT [PK_account] PRIMARY KEY CLUSTERED 
-(
-	[customerid] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table customer*/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE customer(
-	[customerid] [int] NOT NULL,
-	[accountid] [int] NOT NULL,
-	[name] [nvarchar](100) NOT NULL,
-	[phone] [varchar] (20) NOT NULL,
-	[email] [varchar] (100) NULL,
-	[birthday] [date] NULL,
- CONSTRAINT [PK_customer] PRIMARY KEY CLUSTERED 
-(
-	[customerid] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table transferr*/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+	customerid int PRIMARY KEY,
+	accountid int AUTO_INCREMENT,
+	name nvarchar(100) NOT NULL,
+	phone varchar (20) NOT NULL,
+	email varchar (100) NULL,
+	password varchar(50) NOT NULL,
+	money float NOT NULL,
+	loan float(50) NULL,
+	saving float NULL,
+	notiID int NULL
+);
+ALTER TABLE account AUTO_INCREMENT=100000;
+
 CREATE TABLE transferr(
-	[transferid] [int] IDENTITY(1,1) NOT NULL,
-	[account1id] [int] NOT NULL,
-	[account2id] [int] NOT NULL,
-	[password] [varchar](50) NOT NULL,
-	[moneytrans] [float] NOT NULL,
-	[money] [float](50) NOT NULL,
-    	[transfee] [float] NULL,
-	[float] NULL
-	[time] [datetime] NOT NULL,
- CONSTRAINT [PK_transfer] PRIMARY KEY CLUSTERED 
-(
-	[transferid] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table electricity bill*/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+	transferid int PRIMARY KEY AUTO_INCREMENT,
+	account1id int NOT NULL,
+	account2id int NOT NULL,
+	password varchar(50) NOT NULL,
+	moneytrans float NOT NULL,
+	money float(50) NOT NULL,
+    	transfee float NULL,
+	float NULL
+	time datetime NOT NULL
+);
+ALTER TABLE account AUTO_INCREMENT=000001;
+
+
 CREATE TABLE electricitybill(
-	[id] IDENTITY(1,1) NOT NULL,
-	[elecid] [int] NOT NULL,
-    	[periodpay] [date] NULL,
-    	[accountid] [int] NOT NULL,
-	[ownername] [int] NOT NULL,
-	[owneraddress] [nvarchar] (200) NOT NULL,
-	[bill] [float](50) NOT NULL,
-	[status] [bit] NOT NULL,
-	[transfee] [float] NULL,
- CONSTRAINT [PK_account] PRIMARY KEY CLUSTERED 
-(
-	[id]
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+	id IDENTITY(1,1) NOT NULL,
+	elecid int NOT NULL,
+    	periodpay date NULL,
+    	accountid int NOT NULL,
+	ownername int NOT NULL,
+	owneraddress nvarchar (200) NOT NULL,
+	bill float(50) NOT NULL,
+	status bit NOT NULL,
+	transfee float NULL,
+
 /****** Object:  Table water bill*/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE waterbill(
-	[id] IDENTITY(1,1) NOT NULL,
-	[waterid] [int] NOT NULL,
-	[periodpay] [date] NULL,
-   	[accountid] [int] NOT NULL,
-	[ownername] [int] NOT NULL,
-	[owneraddress] [nvarchar] (200) NOT NULL,
-	[bill] [float](50) NOT NULL,
-	[status] [bit] NOT NULL,
-	[transfee] [float] NULL,
- CONSTRAINT [PK_account] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+	id IDENTITY(1,1) NOT NULL,
+	waterid int NOT NULL,
+	periodpay date NULL,
+   	accountid int NOT NULL,
+	ownername int NOT NULL,
+	owneraddress nvarchar (200) NOT NULL,
+	bill float(50) NOT NULL,
+	status bit NOT NULL,
+	transfee float NULL,
+
+
 /****** Object:  Table notification bill*/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE notification(
-	[id] IDENTITY(1000000,1) NOT NULL,
-	[transferid] [date] NULL,
-    	[elecid] [int] NOT NULL,
-	[waterid] [int] NOT NULL,
-	[owneraddress] [nvarchar] (200) NOT NULL,
-	[bill] [float](50) NOT NULL,
-	[status] [bit] NOT NULL,
-	[transfee] [float] NULL,
- CONSTRAINT [PK_account] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+	id IDENTITY(1000000,1) NOT NULL,
+	transferid date NULL,
+    	elecid int NOT NULL,
+	waterid int NOT NULL,
+	owneraddress nvarchar (200) NOT NULL,
+	status bit NOT NULL,
+	transfee float NULL,
 
 
 
 
-ALTER DATABASE [BANK] SET  READ_WRITE
+
+
+ALTER DATABASE BANK SET  READ_WRITE
 GO
